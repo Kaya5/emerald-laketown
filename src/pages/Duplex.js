c311e0a8e91708840dad240104b38b95a1edcd41
Kaya5 <88032824+Kaya5@users.noreply.github.com>
2021-12-11 15:36:42 +0100

    Update Duplex.js

--- src/pages/Duplex.js
+++ src/pages/Duplex.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import "../styles/HouseTypes.css";
-
+import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Image from "../components/Image";
 import PageTitle from "../components/PageTitle";
@@ -18,8 +18,15 @@ function Duplex() {
       <Image title="Central location" link="https://bit.ly/3ib8W3V" />
 
       <div className="homeLinkDiv">
-        <Button href="/" size="large" color="primary" variant='outlined' className="homeLink">
-          Go Home 
+        <Button
+          size="large"
+          color="primary"
+          variant="outlined"
+          className="homeLink"
+        >
+          <Link to="/" className="home-button-text">
+            Go Home
+          </Link>
         </Button>
       </div>
     </div>

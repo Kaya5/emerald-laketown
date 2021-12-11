a65b2fc770af612edd81f69fa7968de7c8d8ed4b
Kaya5 <88032824+Kaya5@users.noreply.github.com>
2021-12-11 15:35:49 +0100

    Update Apartment.js

--- src/pages/Apartment.js
+++ src/pages/Apartment.js
@@ -1,31 +1,35 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import "../styles/HouseTypes.css";
-
 import Button from "@material-ui/core/Button";
 import Image from "../components/Image";
 import PageTitle from "../components/PageTitle";
+import { Link } from "react-router-dom";
 
 function Apartment() {
   return (
-    <div className='houseType'>
+    <div className="houseType">
       <Paper variant="outlined" className="paper">
         <PageTitle text="Apartment" />
       </Paper>
 
-        <Image title="Modern Architecture" link="https://bit.ly/3hWgN4V" />
-        <Image title="Serviced Apartments" link="https://bit.ly/3hOfR2E" />
-        <Image title="State of the art facilities" link="https://bit.ly/3r4QHAX"/>
+      <Image title="Modern Architecture" link="https://bit.ly/3hWgN4V" />
+      <Image title="Serviced Apartments" link="https://bit.ly/3hOfR2E" />
+      <Image
+        title="State of the art facilities"
+        link="https://bit.ly/3r4QHAX"
+      />
 
       <div className="homeLinkDiv">
         <Button
-          href="/"
           size="large"
           color="primary"
           variant="outlined"
           className="homeLink"
         >
-          Go Home
+          <Link to="/" className="home-button-text">
+            Go Home
+          </Link>
         </Button>
       </div>
     </div>

b3060bf46fa5051f3638f9ca3fb73b6df260d1ed
Kaya5 <88032824+Kaya5@users.noreply.github.com>
2021-12-11 15:38:48 +0100

    Update Villa.js

--- src/pages/Villa.js
+++ src/pages/Villa.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import "../styles/HouseTypes.css";
+import { Link } from "react-router-dom";
 
 import { Button } from "@material-ui/core";
 import Image from "../components/Image";
@@ -17,8 +18,15 @@ function Villa() {
       <Image title="Proximity to city centre" link="https://bit.ly/2T0i42y" />
 
       <div className="homeLinkDiv">
-        <Button href="/" size="large" color="primary" variant='outlined' className="homeLink">
-          Go Home
+        <Button
+          size="large"
+          color="primary"
+          variant="outlined"
+          className="homeLink"
+        >
+          <Link to="/" className="home-button-text">
+            Go Home
+          </Link>
         </Button>
       </div>
     </div>
